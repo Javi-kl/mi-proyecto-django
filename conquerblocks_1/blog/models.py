b6fa6097848998df27142e9078a5d34e614ddb66
Javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-03-01 19:54:53 +0100

    feat: Anadir header & footer Crear modelo Post agregar modelo a admin

--- conquerblocks_1/blog/models.py
+++ conquerblocks_1/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="Título", max_length=200)
+    content = models.TextField(verbose_name="Contenido")
+    autor = models.CharField(verbose_name="autor", max_length=100)
+    created_at = models.DateTimeField(
+        verbose_name="Fecha y hora de creacion", default=timezone.now
+    )
+
+    def __str__(self) -> str:
+        return self.title
