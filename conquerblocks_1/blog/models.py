0a56afc304739b5419bde9963b6f7fac564eb928
Javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-03-07 19:51:51 +0100

    feat: agregar thumbnails a lista y detalles del blog

--- conquerblocks_1/blog/models.py
+++ conquerblocks_1/blog/models.py
@@ -1,16 +1,25 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils import timezone
+from thumbnails.fields import ImageField
 
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(verbose_name="Título", max_length=200)
-    content = models.TextField(verbose_name="Contenido")
+    content = RichTextField(verbose_name="Contenido")
     autor = models.CharField(verbose_name="autor", max_length=100)
     created_at = models.DateTimeField(
         verbose_name="Fecha y hora de creacion", default=timezone.now
     )
     show_home = models.BooleanField("Mostrar en la home", default=False)
+
+    blog_img = ImageField(
+        verbose_name="Portada del blog",
+        upload_to="blog/images/",
+        null=True,
+        blank=True,
+    )
 
     def __str__(self) -> str:
         return self.title
